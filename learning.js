8742ba9c0d2691e39d46f20b1b6cd842441ef361
Dmytro Pelyp <pelup19992@gmail.com>
2022-05-10 20:43:27 +0300

    added code

--- learning.js
+++ learning.js
@@ -11,10 +11,24 @@
 // console.log(BILLS);
 
 // const TIPS  = [calculateTips(BILLS[0]), calculateTips(BILLS[1]), calculateTips(BILLS[2])];
+
 // console.log(TIPS);
 
 // const TOTAL_AMOUNT = [BILLS[0] + TIPS[0], BILLS[1] + TIPS[1], BILLS[2] + TIPS[2]]; 
 // console.log(TOTAL_AMOUNT);
+
+// const bills = [31, 95, 276, 540, 27, 205, 11, 1180, 96, 57];
+// const tips = [];
+// const total = [];
+
+// for (let i = 0; i <= bills.length; i++){
+//     const tip  = calculateTips(bills[i]);
+//     tips.push(tip);
+//     const totalBills = calculateTips(bills[i]) + bills[i];
+//     total.push(totalBills)
+// }
+
+// console.log (bills, tips, total)
 
 // console.log(Boolean(0), Boolean(undefined), Boolean(''), Boolean(null), Boolean(NaN));
 
@@ -156,6 +170,9 @@
 //     return averageSales;
 // }
 
+// ok = Math.max(DEPT1_1Q[0],DEPT1_1Q[1],DEPT1_1Q[2])
+// console.log(ok)
+
 // const dept1AverSales1 = getAverage(DEPT1_1Q[0],DEPT1_1Q[1],DEPT1_1Q[2]);
 // const dept2AverSales1 = getAverage(DEPT2_1Q[0],DEPT2_1Q[1],DEPT2_1Q[2]);
 
@@ -165,16 +182,19 @@
 // console.log(dept1AverSales1, dept2AverSales1, dept1AverSales2, dept2AverSales2)
   
 // function printBonus(a, b) {
-//     let bonus = (a - b) / b * 100;
-//     if (bonus >= 30){
-//         console.log(bonus)
-//     } else if(bonus >= 35) {
-//         console.log ('Dept 1 will get a bonus of 35%');
-//     }
+//     let highAverDept = Math.max(a, b);
+//     let lowAverDept  = Math.min(a, b);
+//     let bonus = ((highAverDept - lowAverDept) / lowAverDept) * 100;
+//    if (bonus >= 30){
+//     console.log(`Dept 2 will get a bonus of ${bonus}`)
+// } else if(bonus >= 35) {
+//     console.log (`Dept 2 will get a bonus of ${bonus}`);
+// }
 // }
 
 
 // printBonus(dept1AverSales1, dept2AverSales1);
+// printBonus(dept1AverSales2, dept2AverSales2);
 
 // console.log ('Push up repitition 1')
 // console.log ('Push up repitition 2')
@@ -196,6 +216,86 @@
 //     const ageDiff = fatherSon * 2;
 //     const total = ageDiff - a; 
 //     console.log  (total);
-//   }
+//   };
 
-//   learnJavaScript(58, 23)
+//   learnJavaScript(58, 23);
+
+// const user123 = ['Dimchuk', "Philipcio", 1999, "Tester", ["Lak", "Pes"], false, 123, "OK"];
+
+// const types = [];
+
+// for (let i = 0; i <= user123.length; i++){
+//     console.log(user123[i], typeof(user123[i]));
+//     // types[i] = typeof user123[i];
+//     types.push(typeof user123[i])
+// };
+
+// console.log(types);
+
+// const birthYears = [1999, 1974, 1988, 2004];
+// const ages = [];
+
+// for (let i = 0; i <= birthYears.length; i++){
+//     // ages[i] = birthYears[i];
+//     // const age = (2022 - ages[i]);
+//     ages.push(2021 - birthYears[i])
+//     console.log(ages);
+// };
+
+// const sobachka = ["Lak", 5, true, "Pes", 2.5, false]
+
+// for (let i =0; i <= sobachka.length; i++) {
+//     if (typeof sobachka[i] != 'string') continue;
+
+//     console.log(sobachka[i], typeof sobachka[i])
+// }
+
+// for (let i =0; i <= sobachka.length; i++) {
+//     if (typeof sobachka[i] === 'number') break;
+
+//     console.log(sobachka[i], typeof sobachka[i])
+// }
+
+// const user = ['Dimchuk', "Philipcio", 1999, "Tester", ["Lak", "Pes"], false, 123, "OK"];
+
+// for (let i = user.length -1; i >= 0; i--) {
+//     console.log(user[i]);
+// }
+
+// console.log ("LOOP IN LOOP")
+
+// for (let exercise = 1; exercise <= 3; exercise++){
+//     console.log(`Exercise ${exercise}`)
+
+// for (let rep = 1; rep <= 5; rep++){
+//     console.log(`Ex ${exercise} Reps ${rep}`)
+// }
+// console.log('Out of inner loop')
+// }
+
+// console.log('Out of outer loop')
+
+// console.log("FOR LOOP")
+
+// for (let rep = 1; rep <= 10; rep++){
+//     console.log(`Push ups repetition ${rep}`)
+// }
+
+// console.log("WHILE LOOP")
+
+// let rep =+ 1;
+
+// while (rep <= 10){
+//     console.log(`Push ups repetition ${rep}`);
+//     rep++;
+// }
+
+
+// let x = Math.floor((Math.random() * 100) + 1);
+
+// console.log(x)
+
+// while (x <= 10){
+//     console.log(`Push ups repetition ${x}`);
+//     x++;
+// }
